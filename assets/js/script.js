62dabb9fd1fbe89ce9474baedbd25e7af093bfd4
Erikson <chris.s.erickson@gmail.com>
2022-06-23 16:08:27 -0400

    added new html via submit click with javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,28 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var createTaskHandler =function(event){
 
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name ='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // Create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent ="This is a new task.";
+
+    // create div to hold task infor and add to list item
+    var taskInfoE1 = document.createElement("div");
+    // give the div a class name
+    taskInfoE1.className="task-info";
+
+    // add HTML content to div
+    taskInfoE1.innerHTML="<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+    
+    listItemE1.appendChild(taskInfoE1);
+    
+    // add entire itemto list
     tasksToDoE1.appendChild(listItemE1)
-    console.log(event);
+
 }
 
-formE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
 
 
